f8bb2ce2a9e7cf146c188223e91d8376cb94fbdb
DavidHaitch <dahud.lefthanded@gmail.com>
2014-05-12 19:27:56 -0500

    FM work

--- ToneWand.ino
+++ ToneWand.ino
@@ -25,18 +25,17 @@ Oscil<COS2048_NUM_CELLS, CONTROL_RATE> kIntensityMod(COS2048_DATA);
 
 float smoothness = 0.95;
 Smooth <long> smoothIntensity(smoothness);
-
+Smooth <long> smoothPitch(0.85);
 int amplitude = 0;
 int baseTone = 0;
-int mod_ratio = 6; // brightness (harmonics)
+float mod_ratio = 2; // brightness (harmonics)
 long fm_intensity;
-
+int intensityMultiplier = 100;
 void setup()
 {
-  Serial.begin(57600);
   pinMode(CS_IMU, OUTPUT);
   SPI.begin();
-  
+
   kIntensityMod.setFreq(500);
   startMozzi(CONTROL_RATE); // set a control rate of 64 (powers of 2 please)
 }
@@ -48,38 +47,38 @@ void loop()
 
 int updateAudio()
 {
-    long modulation = smoothIntensity.next(fm_intensity) * aModulator.next();
+  long modulation = smoothIntensity.next(fm_intensity) * aModulator.next();
   return (aCarrier.phMod(modulation) * amplitude)>>8;
 }
 
-int amplitudeMod = 0;
 void updateControl()
 {
   ReadIMU(0);
   int minYaw = -180;
   int maxYaw = 180;
   int shift = 0;
-  
+
   if(ImuToDeg(pitch) < 0)
   {
     shift = 12;
   }
-  
-  baseTone = map(ImuToDeg(yaw), minYaw, maxYaw, 0 + shift, 12 + shift);
-  if(abs(ImuToVel(rolldot)) >= 360)
-  {
-    amplitude = 255;
-  }
 
-  float baseFreq = GetNote(baseTone);
+  baseTone = map(ImuToDeg(yaw), minYaw, maxYaw, 0 + shift, 12 + shift);
+  float baseFreq = smoothPitch.next(GetNote(baseTone));
   int mod_freq = baseFreq * mod_ratio;
   aCarrier.setFreq(baseFreq);
   aModulator.setFreq(mod_freq);
-  fm_intensity = (100 * (kIntensityMod.next()+128))>>8; // shift back to range after 8 bit multiply
+  kIntensityMod.setFreq(16);
+  fm_intensity = kIntensityMod.next()+128; // shift back to range after 8 bit multiply
 
-  amplitude -= 3;
-  if(amplitude > 255) amplitude = 255;
-  if(amplitude < 0) amplitude = 0;
+  amplitude--;
+  
+  if(abs(ImuToVel(rolldot)) >= 360) amplitude = 255;
+  if(amplitude < 1)
+  {
+    amplitude = 1;
+    fm_intensity = 0;
+  }
 }
 
 void ReadIMU(byte cmd)
@@ -110,6 +109,7 @@ float GetNote(int semitoneOffset)
 {
   return 220 * pow(1.059463, semitoneOffset);
 }
+
 
 
 
